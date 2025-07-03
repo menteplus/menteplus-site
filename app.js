1d29805199cf2f796b8824e4b7ffb8f4a725ede7
menteplus <leivis.ml@gmail.com>
2025-07-03 04:03:29 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+
+document.getElementById("root").innerHTML = `
+  <h1>Olá, você é importante! 💛</h1>
+  <p>Essa é a versão web do Mente+ App.</p>
+  <p>Você poderá evoluir, criar metas e cuidar da sua mente aqui.</p>
+`;
